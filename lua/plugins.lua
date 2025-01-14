52c6dca1287c8ba5f3c081a9fed44bf121c06b63
Arthur Rocha de Menezes <stomp@Arthurs-MacBook-Pro.local>
2025-01-14 12:16:40 -0300

    Bump telescope version

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -3,7 +3,7 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
   use 'nvim-lua/plenary.nvim'
   use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.1',
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
     requires = { { 'nvim-lua/plenary.nvim' } }
   }
   use {
